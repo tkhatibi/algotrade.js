f6b560a4514cd52c8e2758a1acfce583f2820812
Touraj Khatibi <2rajpx@gmail.com>
2023-03-05 04:51:06 +0800

    chore: configure eslint, husky, commitlint, lint-staged, prettier and vscode

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+const configConventional = require('@commitlint/config-conventional');
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    ...configConventional.rules,
+    'type-enum': [
+      2,
+      'always',
+      ['hotfix', ...configConventional.rules['type-enum'][2]],
+    ],
+  },
+};
